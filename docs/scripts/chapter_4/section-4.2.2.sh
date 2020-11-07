fbe0eba6be54b01d75195698d7be7981c77c2e1a
omshinde <rajatshinde2303@gmail.com>
2020-11-07 21:26:41 +0530

    Update files with BA osm data

--- docs/scripts/chapter_4/section-4.2.2.sh
+++ docs/scripts/chapter_4/section-4.2.2.sh
@@ -13,7 +13,7 @@ if [ ! -f "@PGR_WORKSHOP_CITY_FILE@.osm" ]; then
 # 4.2.2 from-here
   CITY="@PGR_WORKSHOP_CITY_FILE@"
   wget -N --progress=dot:mega \
-    "http://download.osgeo.org/livedvd/data/osm/$CITY/$CITY.osm.bz2"
+    "http://download.osgeo.org/livedvd/14/osm/$CITY.osm.bz2"
   bunzip2 -f $CITY.osm.bz2
 # 4.2.2 to-here
 
