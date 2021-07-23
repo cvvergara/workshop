9972b54eab3cb453a8ed84ffb1cb3565f2fed24e
cvvergara <vicky@georepublic.de>
2021-07-23 12:10:16 -0500

    This change works on vickys computer

--- docs/scripts/un_sdg/import_mumbai.sh
+++ docs/scripts/un_sdg/import_mumbai.sh
@@ -3,7 +3,7 @@ set -e
 #4.3.1 from-here
 osm2pgrouting \
     -f "mumbai.osm" \
-    -c "../../chapters/code/mapconfig_sample.xml" \
+    -c /usr/local/share/osm2pgrouting/mapconfig.xml \
     -d mumbai \
     -U user \
     -W user \
