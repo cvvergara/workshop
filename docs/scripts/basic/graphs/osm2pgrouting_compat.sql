c3f7730201922dcc53d4dbe370e40e186e5439b6
cvvergara <vicky@erosion.dev>
2026-06-30 12:10:13 -0600

    simplify osm2pgrouting compat scripts: plain ALTER TABLE, no DO/IF

--- docs/scripts/basic/graphs/osm2pgrouting_compat.sql
+++ docs/scripts/basic/graphs/osm2pgrouting_compat.sql
@@ -1,35 +1,6 @@
 -- osm2pgrouting compatibility: rename v2.x columns to v3.x naming
 -- Run this after importing data with osm2pgrouting, before the graphs chapter.
 
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_name = 'ways' AND column_name = 'gid'
-  ) THEN
-    ALTER TABLE ways RENAME COLUMN gid TO id;
-    RAISE NOTICE 'Renamed ways.gid -> id';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_name = 'ways' AND column_name = 'the_geom'
-  ) THEN
-    ALTER TABLE ways RENAME COLUMN the_geom TO geom;
-    RAISE NOTICE 'Renamed ways.the_geom -> geom';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_name = 'ways_vertices_pgr' AND column_name = 'the_geom'
-  ) THEN
-    ALTER TABLE ways_vertices_pgr RENAME COLUMN the_geom TO geom;
-    RAISE NOTICE 'Renamed ways_vertices_pgr.the_geom -> geom';
-  END IF;
-END $$;
+ALTER TABLE ways RENAME COLUMN gid TO id;
+ALTER TABLE ways RENAME COLUMN the_geom TO geom;
+ALTER TABLE ways_vertices_pgr RENAME COLUMN the_geom TO geom;
--- docs/scripts/un_sdg/data/osm2pgrouting_compat_mumbai.sql
+++ docs/scripts/un_sdg/data/osm2pgrouting_compat_mumbai.sql
@@ -1,68 +1,9 @@
 -- osm2pgrouting compatibility: rename v2.x columns to v3.x naming
 -- Run this after importing data with osm2pgrouting for Mumbai.
 
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_schema = 'roads' AND table_name = 'ways' AND column_name = 'gid'
-  ) THEN
-    ALTER TABLE roads.ways RENAME COLUMN gid TO id;
-    RAISE NOTICE 'Renamed roads.ways.gid -> id';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_schema = 'roads' AND table_name = 'ways' AND column_name = 'the_geom'
-  ) THEN
-    ALTER TABLE roads.ways RENAME COLUMN the_geom TO geom;
-    RAISE NOTICE 'Renamed roads.ways.the_geom -> geom';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_schema = 'roads' AND table_name = 'ways_vertices_pgr' AND column_name = 'the_geom'
-  ) THEN
-    ALTER TABLE roads.ways_vertices_pgr RENAME COLUMN the_geom TO geom;
-    RAISE NOTICE 'Renamed roads.ways_vertices_pgr.the_geom -> geom';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_schema = 'buildings' AND table_name = 'ways' AND column_name = 'gid'
-  ) THEN
-    ALTER TABLE buildings.ways RENAME COLUMN gid TO id;
-    RAISE NOTICE 'Renamed buildings.ways.gid -> id';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_schema = 'buildings' AND table_name = 'ways' AND column_name = 'the_geom'
-  ) THEN
-    ALTER TABLE buildings.ways RENAME COLUMN the_geom TO geom;
-    RAISE NOTICE 'Renamed buildings.ways.the_geom -> geom';
-  END IF;
-END $$;
-
-DO $$
-BEGIN
-  IF EXISTS (
-    SELECT 1 FROM information_schema.columns
-    WHERE table_schema = 'buildings' AND table_name = 'ways_vertices_pgr' AND column_name = 'the_geom'
-  ) THEN
-    ALTER TABLE buildings.ways_vertices_pgr RENAME COLUMN the_geom TO geom;
-    RAISE NOTICE 'Renamed buildings.ways_vertices_pgr.the_geom -> geom';
-  END IF;
-END $$;
+ALTER TABLE roads.ways RENAME COLUMN gid TO id;
+ALTER TABLE roads.ways RENAME COLUMN the_geom TO geom;
+ALTER TABLE roads.ways_vertices_pgr RENAME COLUMN the_geom TO geom;
+ALTER TABLE buildings.ways RENAME COLUMN gid TO id;
+ALTER TABLE buildings.ways RENAME COLUMN the_geom TO geom;
+ALTER TABLE buildings.ways_vertices_pgr RENAME COLUMN the_geom TO geom;
